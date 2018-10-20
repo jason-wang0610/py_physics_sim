8a87a9f3b8e2927233e4d3b7f80278db7b8a6497
Jason Wang <pupil.jason.wang@parklane-is.com>
2018-10-20 23:11:37 +0200

    Created Particle class, randomly generates x amount of particles (rand size and x,y)

--- physics_sim.py
+++ physics_sim.py
@@ -1,4 +1,15 @@
 import pygame
+import random
+
+class Particle:
+    def __init__(self, position, size):
+        self.x, self.y = position
+        self.size = size
+        self.colour = (0,0,0)
+        self.thickness = 1
+
+    def display(self):
+        pygame.draw.circle(window, self.colour, (self.x, self.y), self.size, self.thickness)
 
 width, height = 300, 200
 bg_colour = (255,255,255)
@@ -6,6 +17,16 @@ bg_colour = (255,255,255)
 window = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Physics Simulation')
 window.fill(bg_colour)
+
+particle_amount = 10
+particles = []
+for x in range(particle_amount):
+    size = random.randint(10, 20)
+    x, y = random.randint(size, width - size), random.randint(size, height - size)
+    particles.append(Particle((x, y), size))
+
+for particle in particles:
+    particle.display()
 
 pygame.display.flip()
 
